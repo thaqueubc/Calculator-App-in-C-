350887565c9eca7924217c1eb75b307111286a56
Tasnuva Haque <A01174842@ad.bcit.ca>
2019-11-07 16:11:36 -0800

    calculator updated

--- myCalculator/Program.cs
+++ myCalculator/Program.cs
@@ -7,61 +7,88 @@ namespace myCalculator
         static void Main(string[] args)
         {
             // Declare variables and then initialize to zero.
-            int num1 = 0; int num2 = 0;
+            bool close = false;
 
-            // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
-
-            // Ask the user to type the first number.
-            Console.WriteLine("Type a number, and then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            // Ask the user to type the second number.
-            Console.WriteLine("Type another number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            // Ask the user to choose an option.
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            // Use a switch statement to do the math.
-            switch (Console.ReadLine())
+            while (!close)
             {
-                case "a":
-                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "s":
-                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-                    break;
-                case "m":
-                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-                    break;
-                case "d":
-                    while (num2 == 0)
+                double num1 = 0;
+                double num2 = 0;
+
+                // Display title as the C# console calculator app.
+                Console.WriteLine("Console Calculator in C#\r");
+                Console.WriteLine("------------------------\n");
+
+                //Ask the user to type the first number.
+                Console.WriteLine("Type a number, and then press Enter");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+                //Ask the user to type the second number.
+                Console.WriteLine("Type another number, and then press Enter");
+                num2 = Convert.ToDouble(Console.ReadLine());
+          
+                // Ask the user to choose an option.
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+
+                string option = Console.ReadLine();
+
+                    while (option != "a" || option != "s" || option != "m" || option != "d")
                     {
-                        //
-                        //
-                        try
+                        Console.WriteLine("Please choose a valid option");
+                        option = Console.ReadLine();
+
+                        if (option == "a" || option == "s" || option == "m" || option == "d")
                         {
-                            int z = num1 / num2;
-                        }catch(Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                            Console.WriteLine("Enter a non-zero divisor: ");
-                            num2 = Convert.ToInt32(Console.ReadLine());
+                            break;
                         }
+
                     }
-                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-                    break;
+
+                // Use a switch statement to do the math.
+                switch (option)
+                {
+                    case "a":
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "s":
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "m":
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "d":
+                        while (num2 == 0)
+                        {
+                            try
+                            {
+                                double z = num1 / num2;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine("Enter a non-zero divisor: ");
+                                num2 = Convert.ToInt32(Console.ReadLine());
+                            }
+                        }
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                }
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n")
+                {
+                    close = true;
+                }
+                   
+
+           
             }
-            // Wait for the user to respond before closing.
-            Console.Write("Press any key to close the Calculator console app...");
-            Console.ReadKey();
         }
     }
 }
